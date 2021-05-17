abcb3fbb3be13bfe904a9a79a4c9892dd7b823a9
zhisheng Tu <tuzhisheng@gmail.com>
2021-05-17 23:26:39 +0800

    upgrade

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "Apply custom.sh"
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git -b 18.06                                     package/luci-theme-argon-jerrykuku
+git clone --depth 1 https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
+git clone --depth 1 https://github.com/destan19/OpenAppFilter.git                                                  package/luci-app-oaf      
+
 
---
+++ user/lienol-x64-1907/custom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Test custom.sh"
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
+
